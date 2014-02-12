39a790f68918f4ef0f0c17d0e0318dbfbfac009f
codependent <joseantonio.inigo@gmail.com>
2014-02-12 17:56:57 +0100

    koa

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var koa = require('koa');
 var route = require('koa-route');
+var thunkify = require('thunkify');
+var fs = require('fs');
+var readFileThunkified = thunkify(fs.readFile);
+
 var app = koa();
 
 var port = 3000;
 
 // logger
-
 app.use(function *(next){
 	console.log("midd antes")
 	var start = new Date;
@@ -13,6 +16,17 @@ app.use(function *(next){
 	console.log("midd después")
 	var ms = new Date - start;
 	console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// file reader (to test thunks)
+app.use(function *(next){
+	var file1 = yield readFileThunkified('.gitignore');
+	console.log("file1 "+file1);
+  	var file2 = yield readFileThunkified('README.md');
+  	console.log("file2 "+file2);
+	yield next;
+	console.log("files upstream");
+	
 });
 
 app.use(route.get('/', index));
